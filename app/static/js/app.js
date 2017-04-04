fb32ae4e71e45a30be75b7a4b4fbd1fe6a51ba15
softengeek <softengeekkhadz@gmail.com>
2017-04-04 01:30:20 +0000

    Exercise 3

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,8 @@
 // Your JavaScript Code here
+var app = angular.module("thumbsApp", []);
+
+app.controller("thumbsCtrl", function($scope, $http){
+    $http.get('/api/thumbnails').then(function(response){
+        $scope.images = response.data["thumbnails"];
+    });
+});
